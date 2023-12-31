52ef7d18d5e86a8940e32f97a4e03ac12a0a7058
“rizky-eka” <rizky28eka@students.amikom.ac.id>
2024-01-01 01:00:22 +0700

    added all icons svg

--- lib/common/svg.dart
+++ lib/common/svg.dart
@@ -1,8 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
 class AppSVGs {
-  static const String call = 'assets/call.svg';
-  static const String home = 'assets/home.svg';
-  static const String door = 'assets/door.svg';
+  static const String booked = 'assets/icons/booked.svg';
+  static const String bookmark = 'assets/icons/bookmark.svg';
+  static const String call = 'assets/icons/call.svg';
+  static const String card = 'assets/icons/card.svg';
+  static const String cart = 'assets/icons/cart.svg';
+  static const String chat = 'assets/icons/chat.svg';
+  static const String discover = 'assets/icons/discover.svg';
+  static const String email = 'assets/icons/email.svg';
+  static const String history = 'assets/icons/history.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String invoice = 'assets/icons/invoice.svg';
+  static const String menu = 'assets/icons/menu.svg';
+  static const String newchat = 'assets/icons/newchat.svg';
+  static const String newpost = 'assets/icons/newpost.svg';
+  static const String notification = 'assets/icons/notification.svg';
+  static const String scan = 'assets/icons/scan.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String settings = 'assets/icons/settings.svg';
+  static const String shoppingbag = 'assets/icons/shoppingbag.svg';
+  static const String status = 'assets/icons/status.svg';
+  static const String user = 'assets/icons/user.svg';
+  static const String wishlist = 'assets/icons/whishlist.svg';
 }
+
+/*
+
+// Menggunakan SVG dalam widget
+SvgPicture.asset(
+  AppSVGs.call,
+  color: AppColors.primary[500], // Menggunakan warna dari kelas AppColors
+  // Properti widget lainnya...
+)
+
+*/
