5b0c55f3cf23969a3a7b04d125f5948a5f0645fb
“rizky-eka” <rizky28eka@students.amikom.ac.id>
2024-01-11 01:26:05 +0700

    added member image

--- lib/widgets/Card_Slider_widget.dart
+++ lib/widgets/Card_Slider_widget.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+class Member {
+  final String imagePath;
+
+  Member({required this.imagePath});
+}
+
 class MyCardSlider extends StatelessWidget {
-  final List<String> cardList = [
-    'Card 1',
-    'Card 2',
-    'Card 3',
-    'Card 4',
-    'Card 5',
+  final List<Member> memberList = [
+    Member(imagePath: 'marshalenathea1.png'),
   ];
 
   @override
@@ -22,25 +24,45 @@ class MyCardSlider extends StatelessWidget {
         autoPlay: true,
         aspectRatio: 16 / 9,
         viewportFraction: 0.4,
+        autoPlayCurve: Curves.fastOutSlowIn,
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        scrollDirection: Axis.horizontal,
+        scrollPhysics: BouncingScrollPhysics(),
       ),
-      items: cardList.map((card) {
+      items: memberList.map((member) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
               width: 120.0,
               height: 150.0,
               margin: EdgeInsets.symmetric(horizontal: 12.0),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text(
-                  card,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                  ),
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      'assets/images/member/${member.imagePath}',
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned.fill(
+                      child: FractionallySizedBox(
+                        alignment: Alignment.bottomCenter,
+                        heightFactor: 0.24,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.bottomCenter,
+                              end: Alignment.topCenter,
+                              colors: [
+                                Colors.transparent,
+                                Colors.black.withOpacity(0.6),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             );
